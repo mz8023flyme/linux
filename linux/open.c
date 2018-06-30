3cc52785cf6e02b740c455c895199ed64433f8b1
life <2421174533@qq.com>
2018-07-01 07:39:05 +0800

    add the mayun

--- linux/open.c
+++ linux/open.c
@@ -5,7 +5,14 @@
  ************************************************************************/
 
 #include<stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
 int main ()
 {
+    int fd;
 
 }
