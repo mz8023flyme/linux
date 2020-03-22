4a88683f70bb772bb7af60825495aa33f30560dd
clever_beryl <2015942079@qq.com>
2020-03-23 00:01:04 +0800

    updata python pritice
    Signed-off-by: clever_beryl <2015942079@qq.com>

--- python/practice.py
+++ python/practice.py
@@ -1,4 +1,60 @@
 # -*- coding:utf-8 -*-
+import os
+path = os.getcwd()#获取当前路径
+print(path)
+all_files = [f for f in os.listdir(path )]#输出根path下的所有文件名到一个列表中
+#对各个文件进行处理
+print(all_files)
+'''
+class Screen(object):
+    @property
+    def width(self):
+        return self._width
+    @width.setter
+    def width(self,value):
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+    @height.setter
+    def height(self,value):
+        self._height = value
+
+    @property
+    def resolution(self):
+        return self._height * self._width
+# 测试:
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
+'''
+'''
+class Student(object):
+     count = 0
+     def __init__(self, name):
+         self.name = name
+         Student.count +=  1
+ # 测试:
+ if Student.count != 0:
+  print('测试失败1!')
+  else:
+     bart = Student('Bart')
+     if Student.count != 1:
+         print('测试失败2!')
+     else:
+         lisa = Student('Bart')
+         if Student.count != 2:
+             print('测试失败3!')
+         else:
+                print('Students:', Student.count)
+                print('测试通过!')
+'''
 ''' 匿名函数
 L = list(filter(lambda x :x % 2 == 1, range(1, 20)))
 print(L)
